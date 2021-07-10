9cd153df3fb506d7f8edc3d639358fe5cad96285
Ernest muroiwa <emuroiwa@gmail.com>
2021-07-10 13:39:30 +0200

    Update UnitConvertorInterface.php

---
+++ app/UnitConvertor/Contracts/UnitConvertorInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Domain\UnitConvertor\Contracts;
+
+interface UnitConvertorInterface
+{    
+    /**
+     * convert
+     *
+     * @return float
+     */
+    public function convert(): float;  
+      
+    /**
+     * getUnitValue
+     *
+     * @param  mixed $unit
+     * @return float
+     */
+    public function getUnitValue(string $unit): float;
+}
