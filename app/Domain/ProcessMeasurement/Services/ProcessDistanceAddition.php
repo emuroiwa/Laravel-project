f0ca86692491e78a535a0c6169fc6ed4fddf4791
Ernest muroiwa <emuroiwa@gmail.com>
2021-07-10 14:56:30 +0200

    Update ProcessDistanceAddition.php process method

---
+++ app/Domain/ProcessMeasurement/Services/ProcessDistanceAddition.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Domain\ProcessMeasurement\Services;
+
+use App\Domain\UnitConvertor\Services\DistanceConvertor;
+use App\Domain\Calculator\Services\Addition;
+
+/**
+ * ProcessDistanceAddition
+ */
+class ProcessDistanceAddition
+{
+
+    /**
+     * process
+     *
+     * @param  mixed $data
+     * @return array
+     */
+    public function process(array $data): array
+    {
+        $units = [$data['unit1'], $data['unit2']];
+        $values = [$data['value1'], $data['value2']];
+        $returnUnit = $data['return_unit'];
+
+        $results = array_map(null, $units, $values);
+
+        //TODO refactor here
+        $operands = [];
+        foreach ($results as $result) {
+            if ($returnUnit !== $result[0]) {
+                $distanceConvertor = new DistanceConvertor($result[1], $returnUnit, $result[0]);
+                $operands[] = $distanceConvertor->convert();
+            } else {
+                $operands[] = $result[1];
+            }
+        }
+
+        $addition = new Addition;
+        $addition->setOperands($operands);
+        $value = number_format($addition->calculate(), 2);
+        return ['value' => $value, 'unit' => $returnUnit];
+    }
+}
