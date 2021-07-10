026b1f4a76f84053db17bd0fe9db56d5c105cba4
Ernest muroiwa <emuroiwa@gmail.com>
2021-07-10 14:23:01 +0200

    Update AdditionTest.php testing exceptions with test_no_given_operands_throws_exception_when_calculating methods

--- tests/Unit/Calculator/AdditionTest.php
+++ tests/Unit/Calculator/AdditionTest.php
@@ -4,6 +4,7 @@ namespace Tests\Unit\calculator;
 
 use App\Domain\Calculator\Services\Addition;
 use PHPUnit\Framework\TestCase;
+use Exception;
 
 /**
  * AdditionTest
@@ -22,5 +23,19 @@ class AdditionTest extends TestCase
         $addition->setOperands([5, 10, 2]);
 
         $this->assertEquals(17, $addition->calculate());
+    }
+    
+    /**
+     * test_no_given_operands_throws_exception_when_calculating
+     *
+     * @return void
+     */
+    public function test_no_given_operands_throws_exception_when_calculating()
+    {
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('No operands supplied');
+        $addition = new Addition;
+        $addition->setOperands([]);
+        $addition->calculate();
     }
 }
