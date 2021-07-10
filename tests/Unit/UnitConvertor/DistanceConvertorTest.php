1a617bd2a90abaf1f224c6dbc672200b742f71b2
Ernest muroiwa <emuroiwa@gmail.com>
2021-07-10 13:43:12 +0200

    Update DistanceConvertorTest.php docblocks

--- tests/Unit/UnitConvertor/DistanceConvertorTest.php
+++ tests/Unit/UnitConvertor/DistanceConvertorTest.php
@@ -7,17 +7,30 @@ use Illuminate\Foundation\Testing\TestCase;
 use Exception;
 use Tests\CreatesApplication;
 
+/**
+ * DistanceConvertorTest
+ */
 class DistanceConvertorTest extends TestCase
 {
     use CreatesApplication;
-
+    
+    /**
+     * test_converts_given_operands_with_units
+     *
+     * @return void
+     */
     public function test_converts_given_operands_with_units()
     {
         $converted = new DistanceConvertor(4, 'yard', 'metre');
 
         $this->assertEquals(4.374453193350831, $converted->convert());
     }
-
+    
+    /**
+     * test_wrong_units_are_given_throws_exception_when_calculating
+     *
+     * @return void
+     */
     public function test_wrong_units_are_given_throws_exception_when_calculating()
     {
         $this->expectException(Exception::class);
