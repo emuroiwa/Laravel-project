d10dc4649faf0bd7b6b8a174f85318fa57fbf973
Ernest muroiwa <emuroiwa@gmail.com>
2021-07-10 13:28:38 +0200

    Update enums.php for all measurement constants

---
+++ config/enums.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    'length_units' => [
+        'MILLIMETRE'    => 0.001,
+        'CENTIMETRE'    => 0.01,
+        'METRE'         => 1,
+        'KILOMETRE'     => 1000,
+        'INCH'          => 0.0254,
+        'FOOT'          => 0.3048,
+        'YARD'          => 0.9144,
+        'MILE'          => 1609.34,
+        'NAUTICAL_MILE' => 1852,
+    ]
+];
