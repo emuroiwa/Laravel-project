944582e2ff44eaaf7f6a8799193602146db197bf
Ernest muroiwa <emuroiwa@gmail.com>
2021-07-10 15:39:07 +0200

    Update AddDistanceController.php addDistance method

--- app/Http/Controllers/v1/AddDistanceController.php
+++ app/Http/Controllers/v1/AddDistanceController.php
@@ -4,18 +4,23 @@ namespace App\Http\Controllers\v1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\AddDistanceFormRequest;
+use App\Domain\ProcessMeasurement\Services\ProcessDistanceAddition;
+use App\Traits\RespondsWithHttpStatus;
 
 /**
  * AddDistanceController
  */
 class AddDistanceController extends Controller
-{    
+{
+    use RespondsWithHttpStatus;
     /**
      * addDistance
      *
      * @return void
      */
-    public function addDistance(AddDistanceFormRequest $request)
+    public function addDistance(AddDistanceFormRequest $request, ProcessDistanceAddition $addDistance)
     {
+        $result = $addDistance->process($request->all());
+        return $this->success('Distance calculation success', $result);
     }
 }
