0852314374ed13c871dc2b530a6cc2a6d111112c
Ernest muroiwa <emuroiwa@gmail.com>
2021-07-10 14:27:11 +0200

    Update Addition.php handling exceptions

--- app/Domain/Calculator/Services/Addition.php
+++ app/Domain/Calculator/Services/Addition.php
@@ -34,6 +34,9 @@ class Addition implements OperationInterface
      */
     public function calculate(): float
     {
+        if (count($this->operands) === 0) {
+            throw new \Exception('No operands supplied');
+        }
         return array_sum($this->operands);
     }
 }
