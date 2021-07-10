a905ca883900ecc5f43aad70c7e4c2242eab5424
Ernest muroiwa <emuroiwa@gmail.com>
2021-07-10 15:18:38 +0200

    Added AddDistanceController.php

---
+++ app/Http/Controllers/v1/AddDistanceController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\v1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\AddDistanceFormRequest;
+
+/**
+ * AddDistanceController
+ */
+class AddDistanceController extends Controller
+{    
+    /**
+     * addDistance
+     *
+     * @return void
+     */
+    public function addDistance(AddDistanceFormRequest $request)
+    {
+    }
+}
